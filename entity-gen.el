0840662c2af8a4182bb418ac2f905c13ee394a23
Vee Satayamas <vsatayamas@gmail.com>
2017-03-06 15:42:34 +0700

    Support possibly non-public schema

--- entity-gen.el
+++ entity-gen.el
@@ -9,6 +9,10 @@
 ;; Basic usage:
 ;; (gen-entity "table-name")
 ;;
+;; or
+;; 
+;; (gen-entity "table-name" "schema-name")
+;;
 ;; Interactive command:
 ;; entity-gen-generate
 ;;
@@ -20,19 +24,22 @@
 		    :data-type (cadr column)
 		    :column-default (caddr column)))
 
-(defun read-table-info (table-name)
+(defun read-table-info (table-name schema-name)
   (let* ((conn (apply 'pg:connect *db-config*))
 	 (columns (pg:result (pg:exec conn
 				      "SELECT "
 				      "column_name, data_type, column_default "
 				      "FROM information_schema.columns "
 				      "WHERE table_name = "
-				      "'" table-name "'")
+				      "'" table-name "'"
+                                      "and table_schema = "
+                                      "'" schema-name "' ")
+
 			     :tuples)))
     (pg:disconnect conn)
     (mapcar 'list-to-column-info columns)))
 
-(defun read-primary-key-columns (table-name)
+(defun read-primary-key-columns (table-name schema-name)
   (let* ((conn (apply 'pg:connect *db-config*))
 	 (columns (pg:result (pg:exec conn
 				      "SELECT "
@@ -43,7 +50,9 @@
 				      "as tc WHERE "
 				      "constraint_type = 'PRIMARY KEY' "
 				      "and table_name = "
-				      "'" table-name "'")
+				      "'" table-name "' "
+                                      "and table_schema = "
+                                      "'" schema-name "' ")
 			     :tuples)))
     (pg:disconnect conn)
     (mapcar 'car columns)))
@@ -81,14 +90,18 @@
 	     (null (cdr keys)))
     (car keys)))
 
-(defun gen-entity (table-name)
-  (let ((id-col (find-id-column (read-primary-key-columns table-name)))
+(defun gen-entity (table-name &optional schema-name)
+  (when (not schema-name)
+    (setq schema-name "public"))
+  (let ((id-col (find-id-column (read-primary-key-columns table-name schema-name)))
 	(class-name (s-upper-camel-case table-name))
-	(columns (read-table-info table-name)))
+	(columns (read-table-info table-name schema-name)))
     (switch-to-buffer-other-window "*codegen*")
     (erase-buffer)
     (insert "@Entity\n")
-    (insert (format "@Table(name=\"%s\")\n" table-name))
+    (if (equal schema-name "public")
+        (insert (format "@Table(name=\"%s\")\n" table-name))
+      (insert (format "@Table(name=\"%s.%s\")\n" schema-name table-name)))
     (insert (format "class %s {\n" class-name))
     (dolist (col columns)
       (gen-attr col id-col))
